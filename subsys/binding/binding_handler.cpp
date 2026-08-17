2ebf14672981fe7d44f34066b957b26148f33e1d
Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>
2026-08-17 14:36:21 +0200

    subsys: binding: Fix crash when printing unset clusterId
    Check clusterId.has_value() in PrintBindingTable before logging
    unicast bindings, since clusterId is optional in
    Binding::TableEntry.
    
    Signed-off-by: Arkadiusz Balys <arkadiusz.balys@nordicsemi.no>

--- subsys/binding/binding_handler.cpp
+++ subsys/binding/binding_handler.cpp
@@ -26,7 +26,8 @@ void BindingHandler::RunBoundClusterAction(BindingData *bindingData)
 	VerifyOrReturn(bindingData != nullptr, LOG_ERR("Invalid binding data"));
 	VerifyOrReturn(bindingData->InvokeCommandFunc != nullptr, LOG_ERR("No valid InvokeCommandFunc assigned"););
 
-	CHIP_ERROR err = DeviceLayer::PlatformMgr().ScheduleWork(DeviceWorkerHandler, reinterpret_cast<intptr_t>(bindingData));
+	CHIP_ERROR err =
+		DeviceLayer::PlatformMgr().ScheduleWork(DeviceWorkerHandler, reinterpret_cast<intptr_t>(bindingData));
 	VerifyOrReturn(err == CHIP_NO_ERROR, LOG_ERR("ScheduleWork failed: %" CHIP_ERROR_FORMAT, err.Format()));
 }
 
@@ -124,11 +125,14 @@ void BindingHandler::PrintBindingTable()
 			LOG_INF("[%d] UNICAST:", i++);
 			LOG_INF("\t\t+ Fabric: %d\n \
             \t+ LocalEndpoint %d \n \
-            \t+ ClusterId %d \n \
             \t+ RemoteEndpointId %d \n \
             \t+ NodeId %d",
-				(int)entry.fabricIndex, (int)entry.local, (int)entry.clusterId.value(),
-				(int)entry.remote, (int)entry.nodeId);
+				(int)entry.fabricIndex, (int)entry.local, (int)entry.remote, (int)entry.nodeId);
+			if (entry.clusterId.has_value()) {
+				LOG_INF("\t\t+ ClusterId %d", (int)*entry.clusterId);
+			} else {
+				LOG_INF("\t\t+ ClusterId: none");
+			}
 			break;
 		case Binding::MATTER_MULTICAST_BINDING:
 			LOG_INF("[%d] GROUP:", i++);
